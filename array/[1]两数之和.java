c818f2c2a094161423adfdd0b971d9b59527e394
Uncle-Road <457894974@qq.com>
2024-07-18 00:16:04 +0800

    debugging environment

--- array/[1]两数之和.java
+++ array/[1]两数之和.java
@@ -1,4 +1,4 @@
-//给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target 的那 两个 整数，并返回它们的数组下标。 
+package array;//给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target 的那 两个 整数，并返回它们的数组下标。
 //
 // 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。 
 //
@@ -45,6 +45,8 @@
 //
 // Related Topics 数组 哈希表 👍 18722 👎 0
 
+
+import java.util.*;
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
